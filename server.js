7e417deedf8e37ba6dd679f66b598360f1dbaa65
Bounty556 <jacobmayday@gmail.com>
2020-07-31 00:06:36 -0600

    Added html routes

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static('public'));
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout');
+
+// add routes
+app.use(routes);
 
 app.listen(PORT, () => {
   console.log('Listening on http://localhost:' + PORT);
